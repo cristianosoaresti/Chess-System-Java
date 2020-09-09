a1ec118ee9a23469061a06d0a2ecae8de48e8351
Cristiano Soares <cristianosoaresti@hotmail.com>
2020-09-09 19:48:23 -0300

    Class Position created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Word!");
+		
+		Position position = new Position(1, 3);
+		System.out.println(position.toString());
 	}
-
 }
---
+++ src/boardgame/Position.java
@@ -0,0 +1,29 @@
+package boardgame;
+
+public class Position {
+	private int row = 0;
+	private int column = 0;
+
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setValue(int row, int colum) {
+		this.row = row;
+		this.column = colum;
+	}
+	
+	@Override
+	public String toString() {
+		return "Row: " + row + " Column: " + column;
+	}
+}
