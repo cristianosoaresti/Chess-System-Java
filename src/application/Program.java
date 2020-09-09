0743c39c25949a6c41bb99771f6c82690e227ade
Cristiano Soares <cristianosoaresti@hotmail.com>
2020-09-09 20:14:49 -0300

    Classes Board and Piece created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,11 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Position position = new Position(1, 3);
-		System.out.println(position.toString());
+		Board board = new Board(8, 8);
 	}
 }
