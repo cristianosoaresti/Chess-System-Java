7f53bc064a7f0f2cf725ffd6823d3af182c524c3
Cristiano Soares <cristianosoaresti@hotmail.com>
2020-09-23 21:07:26 -0300

    Check Logic (Part 2)

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -35,6 +35,10 @@ public class ChessMatch {
 	public Color getCurrentPlayer() {
 		return currentPlayer;
 	}
+	
+	public boolean getCheck() {
+		return check;
+	}
 
 	private void nextTurn() {
 		turn++;
@@ -65,6 +69,14 @@ public class ChessMatch {
 		validateSourcePosition(source);
 		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
+		
+		if (testCheck(currentPlayer)) {
+			undoMove(source, target, capturedPiece);
+			throw new ChessException("You can not put yourself in check!");
+		}
+		
+		check = (testCheck(opponent(currentPlayer))) ? true : false ;
+		
 		nextTurn();
 		return (ChessPiece) capturedPiece;
 	}
@@ -131,6 +143,18 @@ public class ChessMatch {
 			}
 		}
 		throw new IllegalStateException("There is no " + color + " king on the board");
+	}
+	
+	private boolean testCheck (Color color) {
+		Position kingPosition = king(color).getChessPosition().toPosition();
+		List<Piece> opponentPieces = piecesOnTheBoard.stream().filter(x -> ((ChessPiece) x).getColor() == opponent(color)).collect(Collectors.toList());
+		for(Piece p : opponentPieces) {
+			boolean[][] mat = p.possibleMoves();
+			if (mat[kingPosition.getRow()][kingPosition.getColumn()]) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	private void initialSetup() {
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -21,7 +21,7 @@ public abstract class ChessPiece extends Piece {
 		return ChessPosition.fromPosition(position);
 	}
 
-	protected boolean isThereOpponentPiece(Position position) {
+	protected boolean  isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p != null && p.getColor() != color;
 	}
